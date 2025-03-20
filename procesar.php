cf5844e49a8cf01f43889476f732301cf9e8b1cb
Cristoffer97 <cristofferalcaldebautista@gmail.com>
2025-03-20 15:50:30 +0100

    agregar campos para mostrar

--- procesar.php
+++ procesar.php
@@ -1,7 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = htmlspecialchars($_POST["nombre"]);
+    $correo = htmlspecialchars($_POST["correo"]);
+    $mensaje = htmlspecialchars($_POST["mensaje"]);
 
-    echo "<h1>Hola, $nombre</h1>";
+    echo"<h1>Los datos que has introducido son;</h1>";
+    echo "<h3>El nombre: $nombre</h3>";
+
+    echo "<h3>Tu correo:, $correo</h3>";
+    
+    echo "<h3>Mensaje:, $mensaje</h3>";
+
 }
 ?>
